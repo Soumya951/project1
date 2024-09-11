d77128d1aec1218c28462d5e2efa297c4ec55a29
Soumya951 <150321619+Soumya951@users.noreply.github.com>
2024-09-11 17:20:50 +0530

    Update leapyear.java

--- Assign1/src/Assign1/leapyear.java
+++ Assign1/src/Assign1/leapyear.java
@@ -1,10 +1,10 @@
 package Assign1;
 
-public class lp {
+public class leapyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int year=2023;
+    int year=2024;
     System.out.println((year % 400== 0) ? true : (year % 100 == 0) ? false : (year % 4 == 0) ? true: false);
 	}
 
