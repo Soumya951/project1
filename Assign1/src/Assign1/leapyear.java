80ebe309e9223378ad3ae98de3b98a0b9285169b
Soumya Marella <Soumya Marella@LAPTOP-IDVHC66U>
2024-09-11 16:22:32 +0530

    change

--- Assign1/src/Assign1/leapyear.java
+++ Assign1/src/Assign1/leapyear.java
@@ -4,7 +4,7 @@ public class leapyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int year=2024;
+    int year=2023;
     System.out.println((year % 400== 0) ? true : (year % 100 == 0) ? false : (year % 4 == 0) ? true: false);
 	}
 
